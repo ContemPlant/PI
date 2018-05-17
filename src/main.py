c6b3a051359746ab4e7db4055b7c6c280f183a88
Leonard Eyer <le0nard@me.com>
2018-05-17 15:23:54 +0200

    initial function to stream data from serial to database

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import datetime
 from sensordata import sensorDates
 from graphQLClient import GraphQLClient
 from graphQLQueries import addSensorDataQueryfn
@@ -5,19 +6,35 @@ from graphQLQueries import addSensorDataQueryfn
 # plantsDbEndpoint = 'http://167.99.240.197:8000/graphql'
 plantsDbEndpoint = 'http://localhost:8000/graphql'
 
-for date in sensorDates():
-    print(date)
+client = GraphQLClient(plantsDbEndpoint)
 
-# client = GraphQLClient(plantsDbEndpoint)
 
-# client.executeMultiple(addSensorDataQueryfn, [{
-#     'type': 'TEMP',
-#     'value': '14',
-#     'timestamp': '"2018-05-13T16:28:35+00:00"',
-#     'arduId': '"cjh51fk31001z07497emu90xa"'
-# }, {
-#     'type': 'HUM',
-#     'value': '20',
-#     'timestamp': '"2018-05-13T16:28:35+00:00"',
-#     'arduId': '"cjh51fk31001z07497emu90xa"'
-# }])
+def datesToQuery(date):
+    # unpack all the parameters that we receive
+    flags, pid, aid, timestamp, phealth, psize, temp, hum, rad = date
+
+    # Hard code timestamp and ardu id for now
+    timestamp = datetime.datetime.now().isoformat()
+    aid = "cjh51fk31001z07497emu90xa"
+
+    # Associate values with types
+    types_values = [('TEMP', temp), ('HUM', hum), ('RAD', rad)]
+
+    # Build query vars
+    return [sensorToObj(t, v, timestamp, aid) for t, v in types_values]
+
+
+def sensorToObj(type, value, timestamp, arduId):
+    return {
+        "type": type,
+        "value": value,
+        "timestamp": f'"{timestamp}"',
+        "arduId": f'"{arduId}"'
+    }
+
+# Get all the dates
+for dates in sensorDates():
+    # Build query from dates
+    query = datesToQuery(dates)
+    # Execute
+    client.executeMultiple(addSensorDataQueryfn, query)
