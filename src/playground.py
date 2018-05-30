f9034a7c6c181e1b88650234444ea0baf44ff048
Leonard Eyer <le0nard@me.com>
2018-05-30 15:30:56 +0200

    Parse subscription response to dict

--- src/playground.py
+++ src/playground.py
@@ -1,25 +1,26 @@
-#!/usr/bin/env python
-
-# WS client example
-
-import asyncio
-import websockets
-from graphQLQueries import subscribeToArduChangeQuery
+import asyncio                                          # for async/await
+import websockets                                       # websockets...
+import json                                             # json parsing
+from pprint import pprint                               # pretty printing
+from graphQLQueries import subscribeToArduChangeQuery   # subscrube query
 
 URL = "ws://localhost:8000/subscriptions"
 PROTO = ["graphql-subscriptions"]
 
-async def onPlantLoaded():
 
+async def onPlantLoaded():
     async with websockets.connect(URL, subprotocols=PROTO) as websocket:
         await websocket.send(subscribeToArduChangeQuery())
+        while True:
+            data = await websocket.recv()
+            node = json.loads(data)['payload']['data']['arduChange']['node']
+            yield node
 
-        ret = await websocket.recv()
-        yield ret
 
 async def main():
-
     async for change in onPlantLoaded():
-        print(change)
+        arduID = change['arduId']
+        plant = change['loadedPlant']
+        pprint(plant)
 
 asyncio.get_event_loop().run_until_complete(main())
