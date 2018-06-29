28e9eb069c5d91e1c16e00fe7e1d6113f4dde4e1
Leonard Eyer <le0nard@me.com>
2018-06-29 20:19:23 +0200

    Add heroku backend uri

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,9 @@
 # API address
-API_ADDR = '167.99.240.197:8000'
+API_ADDR = 'contemplantbackend.herokuapp.com'
 # API subscription endpoint
-API_ADDR_SUB = f'ws://{API_ADDR}/subscriptions'
+API_ADDR_SUB = f'wss://{API_ADDR}/subscriptions'
 # API query/mutation endpoint
-API_ADDR_QM = f'http://{API_ADDR}/graphql'
+API_ADDR_QM = f'https://{API_ADDR}/graphql'
 
 # The serial port where the local module is connected to.
 PORT = '/dev/tty.usbserial-DN02MM7G'
